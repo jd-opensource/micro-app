342ac9719202e881a2750467ac5144c442081629
awesomedevin <awesomedevin.deng@gmail.com>
2021-12-20 15:36:02 +0800

    feat: Migrate some code to branch feat-observehead

--- src/create_app.ts
+++ src/create_app.ts
@@ -7,7 +7,6 @@ import type {
   Func,
 } from '@micro-app/types'
 import extractHtml from './source'
-import { commonElementHander } from './source/patch'
 import { execScripts } from './source/scripts'
 import { appStatus, lifeCycles } from './constants'
 import SandBox from './sandbox'
@@ -18,7 +17,6 @@ import {
   isPromise,
   logError,
   isShadowRoot,
-  CompletionPath,
 } from './libs/utils'
 import dispatchLifecyclesEvent, { dispatchUnmountToMicroApp } from './interact/lifecycles_event'
 import globalEnv from './libs/global_env'
@@ -98,32 +96,6 @@ export default class CreateApp implements AppInterface {
     }
   }
 
-  // watch head
-  observeHead (): void {
-    const observeTarget = this.container?.querySelector('micro-app-head')
-    observeTarget?.addEventListener('DOMNodeInserted', (data: Event) => {
-      const child = data.target
-      const { origin } = new URL(this.url)
-      if (child) {
-        // Style tags with data-n-href attributes require special handling - by awesomedevin
-        if (child && child instanceof HTMLStyleElement && child.getAttribute('data-n-href')) {
-          const link = document.createElement('link')
-          link.rel = 'stylesheet'
-          link.type = 'text/css'
-          link.href = CompletionPath(child.getAttribute('data-n-href') || '', this.url)
-          commonElementHander(observeTarget, link, child, globalEnv.rawReplaceChild)
-        } else if (child && child instanceof HTMLLinkElement && child.getAttribute('href') && !child.getAttribute('href')?.match(origin)) {
-          const href = child.getAttribute('href')
-          const link = document.createElement('link')
-          link.rel = 'stylesheet'
-          link.type = 'text/css'
-          link.href = CompletionPath(href || '', this.url)
-          commonElementHander(observeTarget, link, child, globalEnv.rawReplaceChild)
-        }
-      }
-    })
-  }
-
   /**
    * Error loading HTML
    * @param e Error
@@ -170,9 +142,6 @@ export default class CreateApp implements AppInterface {
     cloneNode(this.source.html as Element, this.container as Element, !this.umdMode)
 
     this.sandBox?.start(this.baseroute)
-
-    // Compatible with next dynamic resource loading - by awesomedevin
-    this.observeHead()
 
     let umdHookMountResult: any // result of mount function
 
